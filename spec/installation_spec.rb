d744a8e63e001d60642549f65856c3ca7d55878f
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-02-07 12:20:49 +0100

    Manage package installation

--- spec/installation_spec.rb
+++ spec/installation_spec.rb
@@ -10,7 +10,7 @@ if ['debian', 'ubuntu'].include?(os[:family])
     describe 'Specific Debian and Ubuntu family checks' do
 
         it 'install role packages' do
-            packages = Array[ 'apt' ]
+            packages = Array[ 'php5-apcu' ]
 
             packages.each do |pkg_name|
                 expect(package(pkg_name)).to be_installed
